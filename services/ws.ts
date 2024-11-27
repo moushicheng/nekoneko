3aa68b0e92ba49a1cdeae0fdf2fdac07fb4a9ba6
moshchen <moshchen@tencent.com>
2024-11-27 11:12:08 +0800

    feat: +axios -unuseless code

--- services/ws.ts
+++ services/ws.ts
@@ -84,5 +84,8 @@ export const getWsEntry: () => Promise<WsEntry> = async () => {
     if (!res.data) throw new Error("获取ws连接信息异常");
     console.log("已获取到Ws入口点: " + res?.data?.url);
     return res.data;
+  }).catch((err) => {
+    console.error("获取ws连接信息异常: " + err);
+    throw err
   });
 };
